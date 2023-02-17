91025359a3736548a64a1fb717792230510dafb3
nghiant <nghiant@gviet.vn>
2023-02-17 08:16:19 +0700

    Add Encode, Decode, Sign, Verify functions

--- token.go
+++ token.go
@@ -1,27 +1,217 @@
-package main
+package tokenutil
 
 import (
-	"fmt"
-
-	tokenpb "tdm/tokenutility/protobuf/token"
+	"crypto/hmac"
+	"crypto/sha256"
+	b64 "encoding/base64"
+	"encoding/hex"
+	"errors"
+	jsoniter "github.com/json-iterator/go"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"log"
+	"strings"
+	tokenpb "tdm/tokenutility/protobuf/tokenpb"
 )
 
-func main() {
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
+func Encode(decoded protoreflect.ProtoMessage) (string, error) {
+	out, err := proto.Marshal(decoded)
+
+	if err != nil {
+		return "", err
+	}
+
+	return b64.URLEncoding.EncodeToString([]byte(out)), nil
 }
 
-func Encode[TokenData models.AccessToken | models.RefreshToken](decoded TokenData) string {
-	fmt.Println(configs.GetGeneral())
-	return ""
+func EncodeAT(decoded *tokenpb.AccessToken) string {
+	out, err := Encode(decoded)
+
+	if err != nil {
+		log.Fatalln("Failed to encode access token:", err)
+	}
+
+	return out
 }
 
-func Decode() {
-	fmt.Println(configs.GetGeneral())
+func EncodeRT(decoded *tokenpb.RefreshToken) string {
+	out, err := Encode(decoded)
+
+	if err != nil {
+		log.Fatalln("Failed to encode refresh token:", err)
+	}
+
+	return out
 }
 
-func Sign() {
-	fmt.Println(configs.GetGeneral())
+func Decode(encodedBase64URL string, data protoreflect.ProtoMessage) error {
+	decodedBase64URL, errDecodeBase64 := b64.URLEncoding.DecodeString(encodedBase64URL)
+
+	if errDecodeBase64 != nil {
+		return errDecodeBase64
+	}
+
+	errDecodeProto := proto.Unmarshal([]byte(decodedBase64URL), data)
+
+	if errDecodeProto != nil {
+		return errDecodeProto
+	}
+
+	return nil
 }
 
-func Verify() {
-	fmt.Println(configs.GetGeneral())
+func DecodeAT(encodedBase64URL string) (tokenpb.AccessToken, error) {
+	var at tokenpb.AccessToken
+
+	if err := Decode(encodedBase64URL, &at); err != nil {
+		log.Fatalln("Failed to decode access token - Decode base64:", err)
+		return at, err
+	}
+
+	return at, nil
+}
+
+func DecodeRT(encodedBase64URL string) (tokenpb.RefreshToken, error) {
+	var rt tokenpb.RefreshToken
+
+	if err := Decode(encodedBase64URL, &rt); err != nil {
+		log.Fatalln("Failed to decode refresh token - Decode base64:", err)
+		return rt, err
+	}
+
+	return rt, nil
+}
+
+func DecodeATToJSON(encodedBase64URL string) (string, error) {
+	var at tokenpb.AccessToken
+
+	if err := Decode(encodedBase64URL, &at); err != nil {
+		log.Fatalln("Failed to decode access token - Decode base64:", err)
+		return "", err
+	}
+
+	json, errToJSON := json.Marshal(at)
+
+	if errToJSON != nil {
+		log.Fatalln("Failed to decode access token - To JSON:", errToJSON)
+		return "", errToJSON
+	}
+
+	return string(json), nil
+}
+
+func DecodeRToJSON(encodedBase64URL string) (string, error) {
+	var rt tokenpb.RefreshToken
+
+	if err := Decode(encodedBase64URL, &rt); err != nil {
+		log.Fatalln("Failed to decode refresh token - Decode base64:", err)
+		return "", err
+	}
+
+	json, errToJSON := json.Marshal(rt)
+
+	if errToJSON != nil {
+		log.Fatalln("Failed to decode refresh token - To JSON:", errToJSON)
+		return "", errToJSON
+	}
+
+	return string(json), nil
+}
+
+func DoHMAC(input, secret, outType string) string {
+	// Create a new HMAC by defining the hash type and the key (as byte array)
+	h := hmac.New(sha256.New, []byte(secret))
+
+	// Write Data to it
+	h.Write([]byte(input))
+
+	// Convert to []byte
+	b := h.Sum(nil)
+
+	if outType == "base64" {
+		return b64.URLEncoding.EncodeToString(b)
+	}
+
+	return hex.EncodeToString(b)
+
+}
+
+func SignAT(payload tokenpb.AccessToken, secret string) string {
+
+	encodedAT := EncodeAT(&payload)
+
+	signedAT := DoHMAC(encodedAT, secret, "base64")
+
+	var s strings.Builder
+
+	s.WriteString(encodedAT)
+	s.WriteString(".")
+	s.WriteString(signedAT)
+
+	return s.String()
+}
+
+func SignRT(payload tokenpb.RefreshToken, secret string) string {
+
+	encodedAT := EncodeRT(&payload)
+
+	signedAT := DoHMAC(encodedAT, secret, "base64")
+
+	var s strings.Builder
+
+	s.WriteString(encodedAT)
+	s.WriteString(".")
+	s.WriteString(signedAT)
+
+	return s.String()
+}
+
+func VerifyAT(token, secret string) (tokenpb.AccessToken, error) {
+
+	tokenArr := strings.Split(token, ".")
+
+	payload := tokenArr[0]
+
+	signed := tokenArr[1]
+
+	signedVerify := DoHMAC(payload, secret, "base64")
+
+	if signedVerify != signed {
+		return tokenpb.AccessToken{}, errors.New("invalid token")
+	}
+
+	at, errDecode := DecodeAT(payload)
+
+	if errDecode != nil {
+		log.Fatalln("Failed to decode access token - Decode proto:", errDecode)
+		return tokenpb.AccessToken{}, errDecode
+	}
+
+	return at, nil
+}
+
+func VerifyRT(token, secret string) (tokenpb.RefreshToken, error) {
+
+	tokenArr := strings.Split(token, ".")
+
+	payload := tokenArr[0]
+
+	signed := tokenArr[1]
+
+	signedVerify := DoHMAC(payload, secret, "base64")
+
+	if signedVerify != signed {
+		return tokenpb.RefreshToken{}, errors.New("invalid token")
+	}
+
+	rt, errDecode := DecodeRT(payload)
+
+	if errDecode != nil {
+		log.Fatalln("Failed to decode refresh token - Decode proto:", errDecode)
+		return tokenpb.RefreshToken{}, errDecode
+	}
+
+	return rt, nil
 }
---
+++ token_test.go
@@ -0,0 +1,100 @@
+package tokenutil
+
+import (
+	"google.golang.org/protobuf/proto"
+	"tdm/tokenutility/protobuf/tokenpb"
+	"testing"
+)
+
+var mockAT = tokenpb.AccessToken{
+	Iat:             1676446163,
+	Exp:             1676705363,
+	IsAuthenticated: 1,
+	Uid:             "MWC5ARRF59",
+	Username:        "0945789305",
+	DisplayName:     "094****305",
+	DeviceId:        "1111-2222-3333-4444-5555",
+	DeviceName:      "Samsung",
+	VersionCode:     20200202,
+	Platform:        3,
+	DtId:            999,
+	SpId:            "1",
+	AuthType:        "phone",
+	Plans: []*tokenpb.Plan{{
+		Id:                    "1",
+		Name:                  "test",
+		MaxDeviceLogin:        1,
+		MaxDeviceStream:       1,
+		ExpiredAt:             1234567,
+		ContentIds:            []string{"1", "2", "3"},
+		IsForceMaxDeviceLogin: 0,
+		Platforms:             []int32{1, 2, 3},
+		KplusId:               "kplusid",
+		DeviceId:              "abc",
+		IsAutoRenew:           0,
+	}, {
+		Id:                    "2",
+		Name:                  "test",
+		MaxDeviceLogin:        1,
+		MaxDeviceStream:       1,
+		ExpiredAt:             1234567,
+		ContentIds:            []string{"1", "2", "3"},
+		IsForceMaxDeviceLogin: 0,
+		Platforms:             []int32{1, 2, 3},
+		KplusId:               "kplusid",
+		DeviceId:              "abc",
+		IsAutoRenew:           0,
+	}},
+	ClientId:        "000000002477",
+	HasPackage:      1,
+	IsPaid:          1,
+	Ip:              "113.190.233.178",
+	IsFromVN:        1,
+	City:            "Viet Nam_Ha Noi_Hanoi",
+	Zone:            1,
+	Isp:             "vnpt.com.vn",
+	Jti:             "167644616345584",
+	DeviceInfo:      "",
+	UserAgent:       "",
+	UserCollections: []string{},
+	OsVersion:       "",
+	AppVersion:      "",
+}
+
+var secret = "Bof"
+
+var mockEncoded = "CNOTsp8GENP8wZ8GGAEiCk1XQzVBUlJGNTkqCjA5NDU3ODkzMDUyGDExMTEtMjIyMi0zMzMzLTQ0NDQtNTU1NToHU2Ftc3VuZ0CK9tAJSANg5wdqATFyBXBob25lei0KATESBHRlc3QYASABKIetSzIBMTIBMjIBM0IDAQIDSgdrcGx1c2lkUgNhYmN6LQoBMhIEdGVzdBgBIAEoh61LMgExMgEyMgEzQgMBAgNKB2twbHVzaWRSA2FiY4IBDDAwMDAwMDAwMjQ3N4gBAZABAZoBDzExMy4xOTAuMjMzLjE3OKABAaoBFVZpZXQgTmFtX0hhIE5vaV9IYW5vabABAboBC3ZucHQuY29tLnZuygEPMTY3NjQ0NjE2MzQ1NTg04gEKMDk0KioqKjMwNQ=="
+
+var mockSign = "CNOTsp8GENP8wZ8GGAEiCk1XQzVBUlJGNTkqCjA5NDU3ODkzMDUyGDExMTEtMjIyMi0zMzMzLTQ0NDQtNTU1NToHU2Ftc3VuZ0CK9tAJSANg5wdqATFyBXBob25lei0KATESBHRlc3QYASABKIetSzIBMTIBMjIBM0IDAQIDSgdrcGx1c2lkUgNhYmN6LQoBMhIEdGVzdBgBIAEoh61LMgExMgEyMgEzQgMBAgNKB2twbHVzaWRSA2FiY4IBDDAwMDAwMDAwMjQ3N4gBAZABAZoBDzExMy4xOTAuMjMzLjE3OKABAaoBFVZpZXQgTmFtX0hhIE5vaV9IYW5vabABAboBC3ZucHQuY29tLnZuygEPMTY3NjQ0NjE2MzQ1NTg04gEKMDk0KioqKjMwNQ==.nq1FALgfspL6OmRFq5N550qMQ8SMkSZPpvu7duiKdzQ="
+
+func TestEncodeAT(t *testing.T) {
+	encoded := EncodeAT(&mockAT)
+
+	if encoded != mockEncoded {
+		t.Fatalf("encode fail")
+	}
+}
+
+func TestDecodeAT(t *testing.T) {
+	decoded, _ := DecodeAT(mockEncoded)
+
+	if !proto.Equal(&decoded, &mockAT) {
+		t.Fatalf("decoded fail")
+	}
+}
+
+func TestSignAT(t *testing.T) {
+	signed := SignAT(mockAT, secret)
+
+	if signed != mockSign {
+		t.Fatalf("sign fail")
+	}
+}
+
+func TestVerifyAT(t *testing.T) {
+	signed, _ := VerifyAT(mockSign, secret)
+
+	if !proto.Equal(&signed, &mockAT) {
+		t.Fatalf("verify fail")
+	}
+}
